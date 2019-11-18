186b48d50a94b47c42842eff7e6056f479d17f8a
Victor Basumatary <>
2019-11-18 18:20:49 +0530

    Because netlify

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <main>
       <h1>Am I on Netlify? Surge? Zeit Now?</h1>
-      <p>You tell me!</p>
+      <p>You tell me! Because I don't know! Lol!</p>
     </main>
   );
 }
